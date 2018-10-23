c0cb2278aeab69e6ec40085a0585ecbf67f531b4
Thomas Depke <tdepke@mymail.mines.edu>
2018-10-23 12:00:08 -0600

    Minor refactoring.

--- ClueGame/src/clueGame/Board.java
+++ ClueGame/src/clueGame/Board.java
@@ -28,8 +28,6 @@ public class Board {
 	private String boardConfigFile;
 	private String roomConfigFile;
 	
-	public static final int MAX_BOARD_SIZE = 50;
-	
 	// variable used for singleton pattern
 	private static Board theInstance = new Board();
 	// constructor is private to ensure only one can be created
